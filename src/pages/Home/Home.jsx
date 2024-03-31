bdfcfeabc4d099783d65244fef43c031623ec435
wu-wilson <wilson.cui.wu@gmail.com>
2024-03-31 16:53:56 -0400

    fixed bug related to button animations

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,35 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import Typewriter from "../../components/Typewriter/Typewriter";
 import styles from "./Home.module.scss";
 
 const Home = ({ setPage, animationDone, setAnimationDone }) => {
   const [showButtons, setShowButtons] = useState(false);
 
-  useEffect(() => {
-    if (showButtons) {
-      setAnimationDone(true);
+  const buttonsContainer = useRef(null);
+
+  const enableButtons = () => {
+    if (buttonsContainer) {
+      const buttons = buttonsContainer.current.querySelectorAll("button");
+      buttons.forEach((button) => {
+        button.disabled = false;
+        setAnimationDone(true);
+      });
     }
-  }, [showButtons]);
+  };
+
+  useEffect(() => {
+    const handleAnimationEnd = (event) => {
+      if (event.animationName === styles["fadeIn"]) {
+        enableButtons();
+      }
+    };
+
+    window.addEventListener("animationend", handleAnimationEnd);
+
+    return () => {
+      window.removeEventListener("animationend", handleAnimationEnd);
+    };
+  }, []);
 
   return (
     <div className={styles["container"]}>
@@ -49,12 +69,19 @@ const Home = ({ setPage, animationDone, setAnimationDone }) => {
           </button>
         </div>
       ) : showButtons ? (
-        <div className={styles["buttons"]}>
-          <button onClick={() => setPage("Guided Breathing")}>
+        <div className={styles["buttons"]} ref={buttonsContainer}>
+          <button
+            disabled="disabled"
+            onClick={() => setPage("Guided Breathing")}
+          >
             Guided Breathing
           </button>
-          <button onClick={() => setPage("Forest VR")}>Forest VR</button>
-          <button onClick={() => setPage("Ocean VR")}>Ocean VR</button>
+          <button disabled="disabled" onClick={() => setPage("Forest VR")}>
+            Forest VR
+          </button>
+          <button disabled="disabled" onClick={() => setPage("Ocean VR")}>
+            Ocean VR
+          </button>
         </div>
       ) : null}
     </div>
