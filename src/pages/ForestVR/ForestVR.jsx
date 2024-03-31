72a70a26712c24634777eaf94f0a883458acd214
wu-wilson <wilson.cui.wu@gmail.com>
2024-03-31 19:16:28 -0400

    added temporary VR capability

--- src/pages/ForestVR/ForestVR.jsx
+++ src/pages/ForestVR/ForestVR.jsx
@@ -6,6 +6,7 @@ import { Cloud } from "@react-three/drei";
 import { Grass } from "../../components/Grass/grass";
 import AudioPlayer from "../../components/AudioPlayer/AudioPlayer";
 import styles from "./Forest.module.scss";
+import { VRButton, XR } from "@react-three/xr";
 
 const ForestVR = ({ numTrees, numClouds, setPage }) => {
   // Generate a random position within a range
@@ -19,51 +20,58 @@ const ForestVR = ({ numTrees, numClouds, setPage }) => {
         ◄ Home
       </button>
       <AudioPlayer audioPath="/sounds/forest.mp3" />
+      <VRButton />
       <Canvas shadows>
-        {Array.from({ length: numTrees }).map((_, index) => (
-          <Tree key={index} position={randomPosition(-25, 25, 0)} />
-        ))}
-        {Array.from({ length: numClouds }).map((_, index) => (
-          <Cloud key={index} position={randomPosition(-25, 25, 20)} />
-        ))}
-        <Grass position={[0, 0, 0]} />
-        <mesh
-          name="ground"
-          rotation={[MathUtils.degToRad(-90), 0, 0]}
-          receiveShadow
-        >
-          <planeGeometry args={[50, 50]} />
-          <meshStandardMaterial
-            color={new Color("#3f6d21").convertLinearToSRGB()}
+        <XR>
+          {Array.from({ length: numTrees }).map((_, index) => (
+            <Tree key={index} position={randomPosition(-25, 25, 0)} />
+          ))}
+          {Array.from({ length: numClouds }).map((_, index) => (
+            <Cloud key={index} position={randomPosition(-25, 25, 20)} />
+          ))}
+          <Grass position={[0, 0, 0]} />
+          <mesh
+            name="ground"
+            rotation={[MathUtils.degToRad(-90), 0, 0]}
+            receiveShadow
+          >
+            <planeGeometry args={[50, 50]} />
+            <meshStandardMaterial
+              color={new Color("#3f6d21").convertLinearToSRGB()}
+            />
+          </mesh>
+          <hemisphereLight
+            color="#87CEEB"
+            intensity={0.8}
+            groundColor="#362907"
           />
-        </mesh>
-        <hemisphereLight
-          color="#87CEEB"
-          intensity={0.8}
-          groundColor="#362907"
-        />
-        <ambientLight intensity={0.3} />
-        <directionalLight
-          castShadow
-          position={[2.5, 8, 5]}
-          intensity={0.5}
-          shadow-mapSize-width={1024}
-          shadow-mapSize-height={1024}
-          shadow-camera-far={50}
-          shadow-camera-left={-10}
-          shadow-camera-right={10}
-          shadow-camera-top={10}
-          shadow-camera-bottom={-10}
-        />
-        <pointLight position={[-10, 0, -20]} color="#eef4aa" intensity={0.5} />
-        <PerspectiveCamera
-          far={1000}
-          fov={100}
-          makeDefault
-          near={0.1}
-          position={[0, 3, 6]}
-        />
-        <OrbitControls />
+          <ambientLight intensity={0.3} />
+          <directionalLight
+            castShadow
+            position={[2.5, 8, 5]}
+            intensity={0.5}
+            shadow-mapSize-width={1024}
+            shadow-mapSize-height={1024}
+            shadow-camera-far={50}
+            shadow-camera-left={-10}
+            shadow-camera-right={10}
+            shadow-camera-top={10}
+            shadow-camera-bottom={-10}
+          />
+          <pointLight
+            position={[-10, 0, -20]}
+            color="#eef4aa"
+            intensity={0.5}
+          />
+          <PerspectiveCamera
+            far={1000}
+            fov={100}
+            makeDefault
+            near={0.1}
+            position={[0, 3, 6]}
+          />
+          <OrbitControls />
+        </XR>
       </Canvas>
     </div>
   );
--- src/pages/GuidedBreathing/GuidedBreathing.jsx
+++ src/pages/GuidedBreathing/GuidedBreathing.jsx
@@ -4,6 +4,7 @@ import { Cube } from "../../components/Cube/cube";
 import AudioPlayer from "../../components/AudioPlayer/AudioPlayer";
 import Slider from "../../components/Slider/Slider";
 import styles from "./GuidedBreathing.module.scss";
+import { VRButton, XR } from "@react-three/xr";
 
 const GuidedBreathing = ({ setPage }) => {
   const factor = useRef(0.7);
@@ -17,10 +18,13 @@ const GuidedBreathing = ({ setPage }) => {
         ◄ Home
       </button>
       <AudioPlayer audioPath="/sounds/breathing_music.mp3" />
+      <VRButton />
       <Canvas>
-        <Cube ref={factor} />
-        <ambientLight intensity={0.6} />
-        <directionalLight castShadow position={[2.5, 8, 5]} intensity={0.9} />
+        <XR>
+          <Cube ref={factor} />
+          <ambientLight intensity={0.6} />
+          <directionalLight castShadow position={[2.5, 8, 5]} intensity={0.9} />
+        </XR>
       </Canvas>
     </div>
   );
